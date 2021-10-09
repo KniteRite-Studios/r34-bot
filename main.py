9943aed90800671484e4ef51e520eaa7c0f1b87f
KniteRite Studios <36118010+KniteRite-Studios@users.noreply.github.com>
2021-10-09 23:53:58 +0000

    Added the rule34 code. Added an about command. Added imports. All that jazz

--- main.py
+++ main.py
@@ -1,42 +1,94 @@
 import discord
 import os
+import time
+import requests
+import filecmp
 
 client = discord.Client()
 
+url = "https://rule34.xxx/index.php?page=post&s=list&tags=user%3akniterite+spunow_+"
+
+client.run("ODk1Nzg1MzE3OTM4OTAxMDkz.YV9nFg.DvP-gR2DALs6f0UjXbnB2kb9QT0")
+
+
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-  if message.content.startswith('$hello'):
-      await message.channel.send('Hello!')
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$test'):
+        await message.channel.send('Test Recieved. Command Works!')
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$image'):
+        await message.channel.send(
+            'https://th.bing.com/th/id/OIP.gCTsc0R3s9lwBsiyktkhgAAAAA?w=156&h=159&c=7&r=0&o=5&dpr=1.88&pid=1.7'
+        )
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$embed'):
+        embedVar = discord.Embed(title="Title",
+                                 description="Desc",
+                                 color=0x00ff00)
+        embedVar.add_field(name="Field1", value="Embed Test)", inline=False)
+        embedVar.add_field(name="Field2",
+                           value="Bot made by KniteRite Studios",
+                           inline=False)
+    await message.channel.send(embed=embedVar)
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$about'):
+        await message.channel.send(
+            'Rule34 Update Bot made by KniteRite Studios. Posts when spunow uploads on rule34!'
+        )
 
 
-  if message.author == client.user:
-    return
+r = requests.get(url)
+with open('responsecompare.txt', 'w') as file:
+    file.write(r.text)
+time.sleep(30)
 
-  if message.content.startswith('$test'):
-      await message.channel.send('Test Recieved. Command Works!')
+f1 = open("response.txt", "r")
+f2 = open("responsecompare.txt", "r")
 
+i = 115
+for line1 in f1:
+    i += 1
 
-  if message.author == client.user:
-    return
+    for line2 in f2:
 
-  if message.content.startswith('$image'):
-     await message.channel.send('https://th.bing.com/th/id/OIP.gCTsc0R3s9lwBsiyktkhgAAAAA?w=156&h=159&c=7&r=0&o=5&dpr=1.88&pid=1.7')
-  
- 
-
-  if message.author == client.user:
-    return
-
-  if message.content.startswith('$embed'):
-   embedVar = discord.Embed(title="Title", description="Desc", color=0x00ff00)
-  embedVar.add_field(name="Field1", value="hi", inline=False)
-  embedVar.add_field(name="Field2", value="hi2", inline=False)
-  await message.channel.send(embed=embedVar)
-client.run("ODk1Nzg1MzE3OTM4OTAxMDkz.YV9nFg.DvP-gR2DALs6f0UjXbnB2kb9QT0")
+        # matching line1 from both files
+        if line1 == line2:
+            # print IDENTICAL if similar
+            print("Line ", i, ": IDENTICAL")
+        else:
+            r = requests.get(url)
+        with open('response.txt', 'w') as file:
+            file.write(r.text)
+            embedVar = discord.Embed(title="Title",
+                                     description="Desc",
+                                     color=0x00ff00)
+    embedVar.add_field(
+        name="Field1",
+        value=
+        "A new Rule34 Spunow Post has been uploaded! (or there was a server crash and my bot broke lol)",
+        inline=False)
+    embedVar.add_field(name="Field2", value="hi2", inline=False)
+    embedVar.set_thumbnail(url="https://i.imgur.com/axLm3p6.jpeg")
