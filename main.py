af750f06da60d82e6eb40149b49e7c4b1aa915ef
KniteRite Studios <36118010+KniteRite-Studios@users.noreply.github.com>
2021-10-10 00:15:19 +0000

    Removed useless imports, rearanged url and print tags for testing purposes

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
 import discord
-import os
 import time
 import requests
-import filecmp
-
 client = discord.Client()
 
-url = "https://rule34.xxx/index.php?page=post&s=list&tags=user%3akniterite+spunow_+"
-
-client.run("TOKENHIDDENFORSAFETY")
+client.run("Token")
 
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
+
+url = "https://rule34.xxx/index.php?page=post&s=list&tags=user%3akniterite+spunow_+"
+
+r = requests.get(url)
+with open('responsecompare.txt', 'w') as file:
+    file.write(r.text)
+time.sleep(30)
+
+f1 = open("response.txt", "r")
+f2 = open("responsecompare.txt", "r")
+
+i = 115
+for line1 in f1:
+    i += 1
+
+    for line2 in f2:
+
+        # matching line1 from both files
+        if line1 == line2:
+            # print IDENTICAL if similar
+            print("Line ", i, ": IDENTICAL")
+        else:
+            r = requests.get(url)
+        with open('response.txt', 'w') as file:
+            file.write(r.text)
+            embedVar = discord.Embed(title="Title",
+                                     description="Desc",
+                                     color=0x00ff00)
+    embedVar.add_field(
+        name="Field1",
+        value=
+        "A new Rule34 Spunow Post has been uploaded! (or there was a server crash and my bot broke lol)",
+        inline=False)
+    embedVar.add_field(name="Field2", value="hi2", inline=False)
+    embedVar.set_thumbnail(url="https://rule34.xxx/images/header3c.png")
 
 
 @client.event
@@ -58,37 +89,3 @@ async def on_message(message):
         await message.channel.send(
             'Rule34 Update Bot made by KniteRite Studios. Posts when spunow uploads on rule34!'
         )
-
-
-r = requests.get(url)
-with open('responsecompare.txt', 'w') as file:
-    file.write(r.text)
-time.sleep(30)
-
-f1 = open("response.txt", "r")
-f2 = open("responsecompare.txt", "r")
-
-i = 115
-for line1 in f1:
-    i += 1
-
-    for line2 in f2:
-
-        # matching line1 from both files
-        if line1 == line2:
-            # print IDENTICAL if similar
-            print("Line ", i, ": IDENTICAL")
-        else:
-            r = requests.get(url)
-        with open('response.txt', 'w') as file:
-            file.write(r.text)
-            embedVar = discord.Embed(title="Title",
-                                     description="Desc",
-                                     color=0x00ff00)
-    embedVar.add_field(
-        name="Field1",
-        value=
-        "A new Rule34 Spunow Post has been uploaded! (or there was a server crash and my bot broke lol)",
-        inline=False)
-    embedVar.add_field(name="Field2", value="hi2", inline=False)
-    embedVar.set_thumbnail(url="https://rule34.xxx/images/header3c.png")
